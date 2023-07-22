b5bab35bcd863eed9a7992fb01a4c572fc027069
Marya <2510marycat@gmail.com>
2023-07-23 00:09:11 +0500

    Задача №23

---
+++ Task_23/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+void NumCubes(int number)
+{
+    Console.Write($"{number} -> ");
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write(number*number*number);
+}
+    
+int GetInput(string text)
+    {
+        Console.Write(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+int number = GetInput("Введите число: ");
+NumCubes(number);
